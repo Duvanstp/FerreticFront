73a5755bbbec92a1aa199a4ddd69693e44457a3f
Duvanstp <dizaquita@unal.edu.co>
2022-11-19 20:32:06 -0500

    Verificar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app-routing.module";
 import { LoginComponent } from './login/login.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ApiService} from "./providers/api.service";
+
+// Primeng
+
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
+import {MessageService} from "primeng/api";
 
 
 @NgModule({
@@ -18,9 +26,13 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
-  providers: [],
+  providers: [ApiService,MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder} from "@angular/forms";
 import {ApiService} from "../providers/api.service";
+import {MessageService} from "primeng/api";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
     username:[''],
     password:['']
   })
-  constructor(private fb:FormBuilder, private api:ApiService) { }
+  constructor(private fb:FormBuilder, private api:ApiService,private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
@@ -20,7 +21,11 @@ export class LoginComponent implements OnInit {
     this.api.get_token(this.login_form.value)
       .subscribe(
         data => {
-          console.log(data)
+          if (data!=undefined){
+            this.messageService.add({severity:'success', summary: 'Login', detail: 'Usuario logueado correctamente.'});
+          }else{
+            this.messageService.add({severity:'error', summary: 'Login', detail: 'Usuario o contraseña incorrectos.'});
+          }
         }
       )
   }
